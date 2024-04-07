07596b6ebc8df4f2be0e4b384768eeba3904a733
Wetzstein Philipp <philipp.wetzstein@stud-mail.uni-wuerzburg.de>
2024-04-07 16:56:04 +0200

    Added Control and Instructioncounter

--- src/instructioncounter.h
+++ src/instructioncounter.h
@@ -13,7 +13,39 @@ SC_MODULE(instructioncounter) {
   sc_in<sc_bv<16>> pc_offset;
   sc_out<sc_bv<16>> pc_new;
 
-  SC_CTOR(instructioncounter) {}
+  sc_signal<sc_bv<16>> zw; 
+
+  SC_CTOR(instructioncounter) :clk("clk"), reset("reset"), pcflag("pcflag"), s0("s0"), s1("s1"),
+  pc_offset("pc_offset"), pc_new("pc_new"){
+
+    SC_METHOD(process);
+    sensitive << clk << reset;
+
+    SC_METHOD(concurrent_assignments);
+  }
+
+  void process(){
+    if(clk.posedge()){
+      if(reset){
+        zw.write("0000000000000000");
+      }else if(pcflag){
+        
+        if(!s0 && !s1){
+          sc_int<16> x =(sc_int<16>) zw.read() + (sc_int<16>)pc_offset.read();
+          zw.write(x);
+        }else if(s1 && !s0){
+          sc_int<16> x = (sc_int<16>) zw.read() + 4;
+          zw.write(x);
+        }else if(!s1 && s0){
+          zw.write(pc_offset);
+        }
+      }
+    }
+  }
+
+  void concurrent_assignments(){
+    pc_new.write(zw);
+  }
 };
 
 #endif // INSTRUCTIONCOUNTER_H_
