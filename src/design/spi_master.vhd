bed1d4ea4f3c44d59f4b38d634a2d82425e716b6
Wetzstein Philipp <philipp.wetzstein@stud-mail.uni-wuerzburg.de>
2024-04-16 18:11:10 +0200

    New spi_master

--- src/design/spi_master.vhd
+++ src/design/spi_master.vhd
@@ -7,14 +7,14 @@ entity SPI_Master is
     port
     (
 
-        clk         : in std_logic;                    -- FPGA CLK
-        sclk        : out std_logic;                   -- SPI_CLK                                   
-        mode_select : in std_logic_vector(1 downto 0); -- Read := 0 or Write:= 1 
+        clk         : in std_logic;  -- FPGA CLK
+        sclk        : out std_logic; -- SPI_CLK                                   
+        mode_select : in std_logic;  -- Read := 0 or Write:= 1 
 
         reset : in std_logic;
         mosi  : out std_logic;
         miso  : in std_logic;
-        cs    : in std_logic;                          -- needed for one clk cycle
+        cs    : in std_logic; -- needed for one clk cycle
 
         data_out : out std_logic_vector(31 downto 0);
 
@@ -29,24 +29,21 @@ architecture rtl of SPI_Master is
 
     --constant sclk_modify : integer := 8;                -- To modify sclk
     --variable counter : integer := 1;
-    signal clock_polarity : std_logic := '1';
+    signal clock_polarity : std_logic := '0';
 
     signal data_reg : std_logic_vector(31 downto 0);
-    signal valid    : std_logic;
 
     signal data_in_reg    : std_logic_vector(31 downto 0);
     signal adress_in_reg  : std_logic_vector(15 downto 0);
-    signal cs_counter     : integer := 47;
-    signal mode_select_zw : std_logic_vector(1 downto 0);
+    signal mode_select_zw : std_logic;
 
+    signal write_adress_counter : integer := 15;
+    signal read_adress_counter  : integer := 31;
+
+    type states is (rst, send_adress, write_data, read_data, data);
+    signal currstate : states;
 begin
 
-    data_in_reg    <= data_in when cs = '1'; -- else data_in_reg <= data_in_reg;
-    adress_in_reg  <= addr when cs = '1';
-    data_valid     <= not(valid);
-    sclk           <= clock_polarity;
-    valid          <= '1' when cs = '1' and cs_counter >= 0 else '0';
-    mode_select_zw <= "00" when reset = '1' else "01" when mode_select = "01" and cs = '1' and cs_counter = 47 else "10" when mode_select = "10" and cs = '1';
     -- SCLK Generator
 
     process (clk)
@@ -57,21 +54,20 @@ begin
         if (reset = '1') then
             clock_polarity <= '0';
 
-        elsif (rising_edge(clk) and valid = '1') then
+        elsif (rising_edge(clk)) then
+            --if (currstate /= rst) then
 
             if (sclk_counter = sclk_modif - 1) then
                 clock_polarity <= (clock_polarity xor '1');
                 sclk_counter := 0;
 
-                if (cs_counter >= 0) then
-                    cs_counter      <= cs_counter - 1;
-                else cs_counter <= 47;
-                end if;
             else sclk_counter := sclk_counter + 1;
             end if;
+            --else clock_polarity <= '0';
+            --end if;
 
-        else if (falling_edge(clk) and valid = '1') then
-
+        else if (falling_edge(clk)) then
+            --if (currstate /= rst) then
             if (sclk_counter = sclk_modif - 1) then
                 clock_polarity <= (clock_polarity xor '1');
                 sclk_counter      := 0;
@@ -79,40 +75,76 @@ begin
             end if;
         end if;
     end if;
+    --end if;
 end process;
-
--- Write Phase 
 
 process (clock_polarity)
 
 begin
 
     if (reset = '1') then
-        mosi <= '0';
-    elsif (rising_edge(clock_polarity) and mode_select_zw = "01" and valid = '1') then
-        if (cs_counter > 31 and cs_counter < 47) then
-            mosi            <= addr(cs_counter - 31);
-        elsif (cs_counter <= 31 and cs_counter >= 0) then
-            mosi              <= data_in(cs_counter);
-        end if;
+        currstate <= rst;
     end if;
-end process;
+    if (rising_edge(clock_polarity)) then
 
--- Read Phase
-
-process (clock_polarity)
-
-begin
-    -- send address
-    if (rising_edge(clock_polarity) and mode_select = "10" and valid = '1') then
-        if (cs_counter > 31 and cs_counter < 47) then
-            mosi                 <= addr(cs_counter - 31);
-        elsif (cs_counter    <= 31 and cs_counter >= 0) then
-            data_reg(cs_counter) <= miso;
-        elsif (cs_counter < 0) then
-            data_out <= data_reg;
+        if (currstate = rst) then
+            mosi                 <= '0';
+            data_in_reg          <= (others => '0');
+            adress_in_reg        <= (others => '0');
+            mode_select_zw       <= '0';
+            data_out             <= (others => '0');
+            write_adress_counter <= 15;
+            adress_in_reg        <= addr;
+            data_in_reg          <= data_in;
+            data_reg             <= (others => '0');
+            write_adress_counter <= 15;
+            read_adress_counter  <= 31;
+            data_valid           <= '0';
+            if (cs = '0') then
+                currstate      <= send_adress;
+                mode_select_zw <= mode_select;
+                data_in_reg       <= data_in;
+                adress_in_reg  <= addr;
+            end if;
         end if;
-    end if;
-end process;
+
+        if (currstate = send_adress) then
+            if (write_adress_counter > 0) then
+                mosi                 <= adress_in_reg(write_adress_counter);
+                write_adress_counter <= write_adress_counter - 1;
+            else mosi            <= adress_in_reg(write_adress_counter);
+                if (mode_select_zw = '0') then
+                    currstate <= read_data;
+                elsif (mode_select_zw = '1') then
+                    currstate <= write_data;
+                end if;
+            
+            end if;
+
+        end if;
+
+            if (currstate = read_data) then
+                if (read_adress_counter > 0) then
+                    data_reg(read_adress_counter) <= miso;
+                    read_adress_counter           <= read_adress_counter - 1;
+                else currstate                <= data;
+                end if;
+            end if;
+
+            if (currstate = write_data) then
+                if (read_adress_counter > 0) then
+                    mosi                <= data_in_reg(read_adress_counter);
+                    read_adress_counter <= read_adress_counter - 1;
+                else currstate      <= data;
+                end if;
+            end if;
+
+            if (currstate = data) then
+                data_valid <= '1';
+                data_out   <= data_reg;
+                currstate  <= rst;
+            end if;
+        end if;
+    end process;
 
 end architecture rtl;
